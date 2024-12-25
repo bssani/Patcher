240ddcfec7f01c457f3dcb383a8f1c73026ea9d6
최필립 <vlfflq48@naver.com>
2024-12-26 00:44:06 +0900

    문서 업데이트

--- PatchPlugin/Plugins/Patcher/Source/Patcher/Private/PatchingGameInstance.cpp
+++ PatchPlugin/Plugins/Patcher/Source/Patcher/Private/PatchingGameInstance.cpp
@@ -7,11 +7,12 @@
 #include "AssetRegistry/AssetRegistryModule.h"
 #include "Kismet/GameplayStatics.h"
 
+// 게임 인스턴스가 초기화될 때 호출
 void UPatchingGameInstance::Init()
 {
     Super::Init();
 
-    // Construct PatchVersionURL
+    // 설정에서 PatchVersionURL을 구성
     TArray<FString> TempCdnBaseUrls;
     GConfig->GetArray(TEXT("/Script/Plugins.ChunkDownloader"), TEXT("CdnBaseUrls"), TempCdnBaseUrls, GGameIni);
 
@@ -21,11 +22,11 @@ void UPatchingGameInstance::Init()
         PatchVersionURL = BaseUrl + "/ContentBuildId.txt";
     }
 
-    // create a new Http request and bind the response callback
+    // 패치 버전 정보를 가져오기 위한 HTTP 요청을 생성
     TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
     Request->OnProcessRequestComplete().BindUObject(this, &UPatchingGameInstance::OnPatchVersionResponse);
 
-    // configure and send the request
+    // HTTP 요청을 구성하고 전송
     Request->SetURL(PatchVersionURL);
     Request->SetVerb("GET");
     Request->SetHeader(TEXT("User-Agent"), "X-UnrealEngine-Agent");
@@ -35,64 +36,63 @@ void UPatchingGameInstance::Init()
     }
 }
 
+// 패치 버전 요청에 대한 HTTP 응답의 콜백
 void UPatchingGameInstance::OnPatchVersionResponse(FHttpRequestPtr Request, FHttpResponsePtr response, bool bWasSucessful)
 {
     if (bWasSucessful) {
-        if (!response.IsValid()) { // Pretty important to fix "Assertion failed: IsValid()" when Web Server is down!!!
-            // HTTP request failed
+        if (!response.IsValid()) { 
+            // 응답이 유효하지 않은 경우 처리
             UE_LOG(LogTemp, Display, TEXT("Failed HTTP request response from ContentBuildId.txt file. File Unreachable."));
         }
         else if (EHttpResponseCodes::IsOk(response->GetResponseCode())) {
-            // content build ID. Our Http response will provide this info from txt file. From Blueprint editable variable.
-            FString ContentBuildId = response->GetContentAsString(); // Throws assertion error popup if the CDN is down, because there wasn't a reponse!!!
+            // 응답에서 패치 콘텐츠 ID를 처리
+            FString ContentBuildId = response->GetContentAsString(); 
             UE_LOG(LogTemp, Display, TEXT("Patch Content ID Response: %s"), *ContentBuildId);
-            // initialize the chunk downloader with chosen platform
+            // 현재 플랫폼으로 청크 다운로더를 초기화
             TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetOrCreate();
             Downloader->Initialize(UGameplayStatics::GetPlatformName(), 8);
 
-            // load the cached build ID
+            // 캐시된 빌드 ID를 로드하고 빌드 매니페스트를 업데이트
             Downloader->LoadCachedBuild(DeploymentName);
-
-            // update the build manifest file
+                       
             TFunction<void(bool bSuccess)> UpdateCompleteCallback = [&](bool bSuccess) {bIsDownloadManifestUpToDate = bSuccess; };
             Downloader->UpdateBuild(DeploymentName, ContentBuildId, UpdateCompleteCallback);
         }
         else {
-            // HTTP request error
             UE_LOG(LogTemp, Display, TEXT("Failed HTTP request for ContentBuildId.txt."));
         }
     }
 }
 
+// 게임 인스턴스가 종료될 때 호출
 void UPatchingGameInstance::Shutdown()
 {
     Super::Shutdown();
-    // Shut down ChunkDownloader
+    // 리소스를 해제하기 위해 청크 다운로더를 종료
     FChunkDownloader::Shutdown();
 }
 
+// 다운로드 매니페스트 완료 상태를 업데이트
 void UPatchingGameInstance::OnManifestUpdateComplete(bool bSuccess)
 {
     bIsDownloadManifestUpToDate = bSuccess;
 }
 
+// 로딩 프로세스의 진행 상황을 가져온다
 void UPatchingGameInstance::GetLoadingProgress(int32& BytesDownloaded, int32& TotalBytesToDownload, float& DownloadPercent, int32& ChunksMounted, int32& TotalChunksToMount, float& MountPercent) const
 {
-    //Get a reference to ChunkDownloader
+    // 청크 다운로더 인스턴스를 가져온다
     TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetChecked();
 
-    //Get the loading stats struct
     FChunkDownloader::FStats LoadingStats = Downloader->GetLoadingStats();
 
-    //Get the bytes downloaded and bytes to download
+    // 다운로드와 마운트 진행률을 계산
     BytesDownloaded = LoadingStats.BytesDownloaded;
     TotalBytesToDownload = LoadingStats.TotalBytesToDownload;
 
-    //Get the number of chunks mounted and chunks to download
     ChunksMounted = LoadingStats.ChunksMounted;
     TotalChunksToMount = LoadingStats.TotalChunksToMount;
 
-    //Calculate the download and mount percent using the above stats
     if (BytesDownloaded != 0 || TotalBytesToDownload != 0)
     {
         DownloadPercent = ((float)BytesDownloaded / (float)TotalBytesToDownload) * 100.0f;
@@ -114,18 +114,19 @@ void UPatchingGameInstance::GetLoadingProgress(int32& BytesDownloaded, int32& To
     }
 }
 
+// 게임 패치 프로세스를 시작
 bool UPatchingGameInstance::PatchGame()
 {
-    // make sure the download manifest is up to date
+    // 청크 다운로더 인스턴스를 가져오기
     if (bIsDownloadManifestUpToDate)
     {
-        // get the chunk downloader
+        
         TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetChecked();
 
-        // This might not be necessary since QueryDB runs at begin play.
+        // 다운로드할 모든 청크 ID를 가져오기
         Downloader->GetAllChunkIds(ChunkDownloadList);
 
-        // report manifest file's chunk status
+        // 청크 상태를 로그에 기록
         for (int32 ChunkID : ChunkDownloadList)
         {
             int32 ChunkStatus = static_cast<int32>(Downloader->GetChunkStatus(ChunkID));
@@ -133,10 +134,11 @@ bool UPatchingGameInstance::PatchGame()
 
         }
 
+        // 청크를 다운로드하고 로딩 모드를 시작
             TFunction<void(bool bSuccess)> DownloadCompleteCallback = [&](bool bSuccess) {OnDownloadComplete(bSuccess); };
             Downloader->DownloadChunks(ChunkDownloadList, DownloadCompleteCallback, 1);
 
-            // start loading mode. This outside of the if might fix Assertion failed: !"Pak cannot be unmounted with outstanding requests" 
+           
             TFunction<void(bool bSuccess)> LoadingModeCompleteCallback = [&](bool bSuccess) {OnLoadingModeComplete(bSuccess); };
             Downloader->BeginLoadingMode(LoadingModeCompleteCallback);
         
@@ -144,29 +146,32 @@ bool UPatchingGameInstance::PatchGame()
         return true;
     }
 
-    // you couldn't contact the server to validate your Manifest, so you can't patch
+    // 매니페스트가 최신 상태가 아니면 패치할 수 없다
     UE_LOG(LogTemp, Display, TEXT("Manifest Update Failed. Can't patch the game"));
 
     return false;
 }
 
+// 로딩 모드가 완료되었을 때 호출
 void UPatchingGameInstance::OnLoadingModeComplete(bool bSuccess)
 {
     OnDownloadComplete(bSuccess);
 }
 
+// 모든 청크가 마운트되었을 때 호출
 void UPatchingGameInstance::OnMountComplete(bool bSuccess)
 {
     OnPatchComplete.Broadcast(bSuccess);
 }
 
+// 청크 다운로드 프로세스가 완료되었을 때 호출
 void UPatchingGameInstance::OnDownloadComplete(bool bSuccess)
 {
     if (bSuccess)
     {
         UE_LOG(LogTemp, Display, TEXT("Download complete"));
 
-        // get the chunk downloader
+        // 다운로드된 청크를 마운트
         TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetChecked();
         FJsonSerializableArrayInt DownloadedChunks;
 
@@ -175,7 +180,6 @@ void UPatchingGameInstance::OnDownloadComplete(bool bSuccess)
             DownloadedChunks.Add(ChunkID);
         }
 
-        //Mount the chunks
         TFunction<void(bool bSuccess)> MountCompleteCallback = [&](bool bSuccess) {OnMountComplete(bSuccess); };
         Downloader->MountChunks(DownloadedChunks, MountCompleteCallback);
 
@@ -185,7 +189,6 @@ void UPatchingGameInstance::OnDownloadComplete(bool bSuccess)
     {
         UE_LOG(LogTemp, Display, TEXT("Load process failed"));
 
-        // call the delegate
         OnPatchComplete.Broadcast(false);
     }
 }
--- PatchPlugin/Plugins/Patcher/Source/Patcher/Private/PatchingGameInstance.h
+++ PatchPlugin/Plugins/Patcher/Source/Patcher/Private/PatchingGameInstance.h
@@ -15,7 +15,6 @@ class UPatchingGameInstance : public UGameInstance
 	GENERATED_BODY()
 
 public:
-    // Overrides
     virtual void Init() override;
     virtual void Shutdown() override;
     const FString DeploymentName = "PatchingCDN"; // DefaultGame.ini  /Script/Plugins.ChunkDownloader 에 있는 이름
@@ -25,44 +24,32 @@ public:
     void GetLoadingProgress(int32& BytesDownloaded, int32& TotalBytesToDownload, float& DownloadPercent, int32& ChunksMounted, int32& TotalChunksToMount, float& MountPercent) const;
 
 public:
-    // Delegates
-    // Fired when the patching process succeeds or fails
     UPROPERTY(BlueprintAssignable, Category = "Patching");
     FPatchCompleteDelegate OnPatchComplete;
 
 public:
-    // Starts the game patching process. Returns false if the patching manifest is not up to date. */
     UFUNCTION(BlueprintCallable, Category = "Patching")
     bool PatchGame();
 
     void OnPatchVersionResponse(FHttpRequestPtr Request, FHttpResponsePtr response, bool bWasSucessful);
 
 protected:
-    //Tracks if our local manifest file is up to date with the one hosted on our website
     UPROPERTY(BlueprintReadOnly, Category = "Patching");
     bool bIsDownloadManifestUpToDate;
 
 protected:
-    //Called when the chunk download process finishes
     void OnManifestUpdateComplete(bool bSuccess);
 
 protected:
-    // List of Chunk IDs to try and download
     TArray<int32> ChunkDownloadList;
-
-    // Base URL for the Deployment
     FString BaseUrl;
-    // URL variable to ContentBuildId.txt file
     FString PatchVersionURL;
 
 protected:
-    // Called when the chunk download process finishes
     void OnDownloadComplete(bool bSuccess);
 
-    // Called whenever ChunkDownloader's loading mode is finished
     void OnLoadingModeComplete(bool bSuccess);
 
-    // Called when ChunkDownloader finishes mounting chunks
     void OnMountComplete(bool bSuccess);
 
 };
